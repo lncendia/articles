6b5b79e37f854e98e03621eb52a2a6e459397533
Gobba-byte <4lucarda@gmail.com>
2025-05-26 20:26:17 +0700

    update GetArticlesInSection

--- Articles.Infrastructure.Web/Catalogs/Controllers/CatalogController.cs
+++ Articles.Infrastructure.Web/Catalogs/Controllers/CatalogController.cs
@@ -1,11 +1,16 @@
+using Articles.Application.Abstractions.DTOs.Articles;
+using Articles.Application.Abstractions.DTOs.Common;
+using Articles.Application.Abstractions.Queries.Catalogs;
 using Articles.Infrastructure.Web.Catalogs.InputModels;
+using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Articles.Infrastructure.Web.Catalogs.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class CatalogController : ControllerBase
+public class CatalogController(ISender mediator, IMapper mapper) : ControllerBase
 {
     // Получить список разделов
     [HttpGet]
@@ -16,8 +21,10 @@ public class CatalogController : ControllerBase
 
     // Получить список статей в разделе по тэгам
     [HttpGet("Articles")]
-    public IActionResult GetArticlesInSection([FromBody] SectionTagsRequest request)
+    public async Task<CountResult<ArticleDto>> GetArticlesInSection([FromBody] SectionTagsRequest request, CancellationToken token)
     {
-        return Ok();
+        var query = mapper.Map<SectionTagsQuery>(request);
+        
+        return await mediator.Send(query, token);
     }
 }
