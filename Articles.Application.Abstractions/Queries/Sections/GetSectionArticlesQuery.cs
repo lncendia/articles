53fff787f1a6db4abc36af3a3a57a0ed7919bb6f
Incendia <egor.lazeba@yandex.ru>
2025-05-26 22:01:25 +0300

    /

--- Articles.Application.Abstractions/Queries/Sections/GetSectionArticlesQuery.cs
+++ Articles.Application.Abstractions/Queries/Sections/GetSectionArticlesQuery.cs
@@ -5,22 +5,27 @@ using MediatR;
 namespace Articles.Application.Abstractions.Queries.Sections;
 
 /// <summary>
-/// Команда запрашивающая.
+/// Запрос на получение статей в разделе с пагинацией
 /// </summary>
+/// <remarks>
+/// Содержит параметры для постраничной выборки статей конкретного раздела
+/// </remarks>
 public class GetSectionArticlesQuery : IRequest<CountResult<ArticleDto>>
 {
     /// <summary>
-    /// Коллекция уникальных тегов.
+    /// Идентификатор раздела
     /// </summary>
     public required Guid SectionId { get; init; }
     
     /// <summary>
-    /// 
+    /// Количество пропускаемых элементов
     /// </summary>
+    /// <value>Минимальное значение: 0</value>
     public required int Skip { get; init; }
     
     /// <summary>
-    /// 
+    /// Количество получаемых элементов
     /// </summary>
+    /// <value>Минимальное значение: 1, Максимальное значение: 100</value>
     public required int Take { get; init; }
 }
